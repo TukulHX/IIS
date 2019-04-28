e26eefad9d06a5d14ce9defc3b85e6f1d538933d
LuMing <784315443@qq.com>
2019-04-28 21:48:42 +0800

    [feature]  trigger 模块 1、实现大文件的传送 2、修改缩进以适应 python3 在发送大文件之前先发送其大小，再利用 base64 将字节流转化为字符流进行发送 注意必须用 struct 模块将 int 转化为 4 字节流，且需要使用大端以适应网络传送

--- server/server.py
+++ server/server.py
@@ -4,23 +4,26 @@ import json
 import os
 import sys
 import platform
-import thread
 import time
+import base64
+import struct
 if sys.version_info.major is 3:
 	import socketserver
 	from json.decoder import JSONDecodeError
+	import _thread as thread
 else:
 	import SocketServer as socketserver
+	import thread
 from libs.helper import *
 config = None
 
 def trigger_thread(event, invoke, outpath):
-        print("start new thread", event, invoke, outpath)
-	event_stream = os.popen( event )   # start event
-	ret = event_stream.read()                       # may be block
-	invoke_stream = os.popen( invoke )   # return saved file path
+	print("start new thread", event, invoke, outpath)
+	event_stream = os.popen( event )	# start event
+	ret = event_stream.read()		# may be block
+	invoke_stream = os.popen( invoke )	# return saved file path
 	file_path = invoke_stream.read()
-        localtime = time.strftime("%y_%m_%d_%H_%M_%S")
+	localtime = time.strftime("%y_%m_%d_%H_%M_%S")
 	with open( outpath + localtime ,'w') as f:
 		f.write(file_path)
 
@@ -44,13 +47,13 @@ class MyServer(socketserver.BaseRequestHandler):
 		print(respons,'sended')
 		flag = True
 		while flag:
-                        respons = {}
+			respons = {}
 			raw_data = conn.recv(1024).decode()
 			try:
 				data = json.loads(raw_data)
 			except ValueError:
 				print('Json Decode error')
-                                break
+				break
 			print(data,'receive')
 			if sys.version_info.major is 3:
 				component = config[data['name']]
@@ -71,22 +74,23 @@ class MyServer(socketserver.BaseRequestHandler):
 
 			elif component['type'] == 'trigger':
 				if(data['value'] == 'start'):
-			            thread.start_new_thread(trigger_thread,(component['event'], component['invoke'], component['path']))	
-		                    respons = {'content':'success'}
-                                elif data['value'] == 'fetch':
-                                    num = 0
-                                    for log in os.listdir(component['path']):
-                                        with open(component['path'] + log, 'r') as f:
-                                            file = f.read()
-                                            with open(file,'r') as f2:
-                                                print("luming",type(respons),respons)
-                                                respons['extra'+ str(num)] = f2.read()
-                                            respons['name'+ str(num)] = file.split('/')[-1] ## get file name
-                                        num = num + 1
-                                        os.remove(component['path'] + log)
-                                    respons['content'] = num
-
-                        respons = json.dumps(respons)
+					thread.start_new_thread(trigger_thread,(component['event'], component['invoke'], component['path']))
+					respons = {'content':'success'}
+				elif data['value'] == 'fetch':
+					num = 0
+					for log in os.listdir(component['path']):
+						with open(component['path'] + log, 'r') as f:
+							file = f.read()
+							with open(file,'rb') as f2:
+								respons['extra'+ str(num)] = base64.b64encode(f2.read())
+							respons['name'+ str(num)] = file.split('/')[-1] ## get file name
+							num = num + 1
+						os.remove(component['path'] + log)
+					respons['content'] = num
+				size = len(json.dumps(respons))
+				print("fetch size is", size)
+				conn.sendall(struct.pack('>I',size))
+			respons = json.dumps(respons)
 			conn.sendall(respons.encode())
 			print(respons,'sended')
 
